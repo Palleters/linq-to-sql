1c85da389e25e8209259d70849544185cc3e7d00
Magnus Hiie <magnus.hiie@gmail.com>
2017-02-26 19:56:45 +0200

    Fix missing importsd

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export * from './src/sql-fragment';
 export * from './src/expressions';
 export * from './src/record-expressions';
 export * from './src/interfaces';
+export * from './src/sql';
+export * from './src/object';
